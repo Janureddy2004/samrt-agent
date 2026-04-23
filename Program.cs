4deb5d8b48d18f9c53777d48d25ca644a21dff05
Janu Reddy2004 <reddyjanu01@gmail.com>
2026-04-23 19:01:47 +0530

    Initial commit - Smart Agent

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AgentPractice.Agents;
+class Program
+{
+    static async Task Main()
+    {
+        var agent = new Agent();
+
+        while (true)
+        {
+            Console.Write("You: ");
+            var input = Console.ReadLine();
+
+            var response = await agent.Run(input);
+
+            Console.WriteLine("Agent: " + response);
+        }
+    }
+}
